ae767707a0fa8018869728e166b28729056b1e43
dam1 <dam1@Mac-Mini-07.local>
2023-11-23 14:29:34 +0100

    lol

--- Programación/ejercicios_java_metodos/ejercicios_java_metodos/metodos/src/Ejer_07_metodos.java
+++ Programación/ejercicios_java_metodos/ejercicios_java_metodos/metodos/src/Ejer_07_metodos.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
+
 public class Ejer_07_metodos {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int veces=sc.nextInt();
+        suma(veces);
+        sc.close();
+    }
+
+    public static double suma(int veces){
+        for(int i=0;i<veces;i++){
+            System.out.println("modulo ejecutandose");
+        }
+    }
+    public static double resta(int veces){
+        for(int i=0;i<veces;i++){
+            System.out.println("modulo ejecutandose");
+        }
+    }
+    public static double multiplicacion(int veces){
+        for(int i=0;i<veces;i++){
+            System.out.println("modulo ejecutandose");
+        }
+    }
+    public static double division(int veces){
+        for(int i=0;i<veces;i++){
+            System.out.println("modulo ejecutandose");
+        }
+    }
 }
