75c7a0cce1db318f42c53176f8b5f30847127293
julian <juliangamezfernandez4@gmail.com>
2023-11-26 00:56:32 +0100

    loll

--- Programación/ejercicios_java_metodos/ejercicios_java_metodos/metodos/src/Ejer_07_metodos.java
+++ Programación/ejercicios_java_metodos/ejercicios_java_metodos/metodos/src/Ejer_07_metodos.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 
 public class Ejer_07_metodos {
     public static void main(String[] args) {
+        double resultado;
         Scanner sc = new Scanner(System.in);
-        System.out.println("dame dos numeros enteros");
+        System.out.println("Ahora elige que cuenta quieres hacer con estos simbolos( + - / * )");
         
+        String sim=sc.nextLine();
+        System.out.println("dame dos numeros enteros");
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
-        double resultado;
-        
-        System.out.println("Ahora elige que cuenta quieres hacer con estos simbolos( + - / * )");
-        String sim=sc.nextLine();
         if (sim.equals("+")){
             resultado =suma(num2,num1);
         }else if (sim.equals("-")){
--- Programación/ejercicios_java_metodos/ejercicios_java_metodos/metodos/src/Ejer_08_metodos.java
+++ Programación/ejercicios_java_metodos/ejercicios_java_metodos/metodos/src/Ejer_08_metodos.java
@@ -1,3 +1,33 @@
+import java.util.Scanner;
 public class Ejer_08_metodos {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double euros;
+        System.out.println("Indique cuantos euros quieres cambiar");
+        euros=sc.nextInt();
+        System.err.println("¿a que moneda desa cambiarlos?");
+        String eleccion = sc.nextLine();
+        char letra = eleccion.charAt(0);
+        do
+            if(letra=='l' || letra == 'L'){
+                libras(euros);
+
+            }else if(letra=='d' || letra == 'D'){
+                dolares(euros);
+            
+            }else if(letra=='y' || letra == 'Y'){
+                yenes(euros);
+                
+            }
+        while (letra!='y' && letra =='Y' && letra =='D'&&letra!='d'&& letra =='l'&&letra!='L');
+    public static void yenes(double euros){
+        System.out.println("hola");
+    }
+    public static void dolares(double euros){
+        System.out.println("hola");
+    }
+    public static void libras(double euros){
+        System.out.println("hola");
+    }
+    }
 }
