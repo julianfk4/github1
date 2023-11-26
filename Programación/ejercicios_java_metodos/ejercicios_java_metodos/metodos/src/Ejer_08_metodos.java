735baafbea19547a59ef844327fe986fbc3d3777
julian <juliangamezfernandez4@gmail.com>
2023-11-26 03:43:13 +0100

    3:43

--- Programación/ejercicios_java_metodos/ejercicios_java_metodos/metodos/src/Ejer_08_metodos.java
+++ Programación/ejercicios_java_metodos/ejercicios_java_metodos/metodos/src/Ejer_08_metodos.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 public class Ejer_08_metodos {
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double euros;
         System.out.println("Indique cuantos euros quieres cambiar");
         euros=sc.nextInt();
         System.err.println("¿a que moneda desea cambiarlos?");
-        String eleccion = sc.nextLine();
+        String eleccion = sc.next();
         char letra = eleccion.charAt(0);
         do
             if(letra=='l' || letra == 'L'){
@@ -17,9 +18,8 @@ public class Ejer_08_metodos {
                 yenes(euros);
             }
         while (letra!='y' && letra =='Y' && letra =='D'&&letra!='d'&& letra =='l'&&letra!='L');
-
     public static void libras(double euros){
-        System.out.println("hola");
+            System.out.println("hoal");
     }
     public static void dolares(double euros){
         System.out.println("hola");
@@ -27,5 +27,6 @@ public class Ejer_08_metodos {
     public static void yenes(double euros){
         System.out.println("adios");
     }
+    
     }
 }
